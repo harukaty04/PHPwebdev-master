6d0d4282f7acd5fbfa6287423b9ed0cb1fd1008c
harukaty04 <haruka.kimura04@gmail.com>
2020-11-26 23:32:39 +0900

    htmlspecialcharsの使用

---
+++ work/web/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = 'Taro<script>alert(1);</script>';
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Practice</title>
+</head>
+<body>
+  <p>Hello, <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8'); ?>!</p>
+</body>
+</html>
