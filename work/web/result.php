1f3d612de20db46fed2d98bb0ca7bab721ad311f
harukaty04 <haruka.kimura04@gmail.com>
2020-11-27 00:21:40 +0900

    create form for send

--- work/web/result.php
+++ work/web/result.php
@@ -6,11 +6,10 @@ require('../app/functions.php');
 include('../app/_parts/_header.php');
 
 ?>
+<p><?= h($message); ?></p>
+<p><a href="index.php">Go back</a></p>
 
-<form action="result.php" method="get">
-  <input type="text" name="message"> 
-  <button>Send</button>
-</form>
+
 <?php
 
 include('../app/_parts/_footer.php');
