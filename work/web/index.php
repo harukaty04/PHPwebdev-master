a6772cc0833462563c33972354d63efd0741950d
harukaty04 <haruka.kimura04@gmail.com>
2020-11-27 16:52:44 +0900

    セレクトボックスから値を受け取る

--- work/web/index.php
+++ work/web/index.php
@@ -8,7 +8,11 @@ include('../app/_parts/_header.php');
 ?>
 
 <form action="result.php" method="get">
-<textarea name="message"></textarea>
+  <select name="color">
+    <option value="orange">Orange</option>
+    <option value="pinke">Pink</option>
+    <option value="gold">Gold</option>
+  </select>
   <button>Send</button>
 </form>
 <?php
